6f24e06e42800babf26dc4a99bbb9ce62664062b
ArssGoetia <lalon742@gmail.com>
2021-10-19 21:32:07 -0500

    Menu de inicio y menu de desplazamiento

---
+++ app/src/main/java/com/arssg/proyecto_odisea_xxi/pantalla_menu.java
@@ -0,0 +1,14 @@
+package com.arssg.proyecto_odisea_xxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantalla_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_menu);
+    }
+}

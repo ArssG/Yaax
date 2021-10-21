9e61c77ba38e89c1cfe74951c220c6e71560af34
AbdelBustamanteD <vi14204@innovaccion.mx>
2021-10-21 16:36:07 -0500

    Login-SQLite

--- app/src/main/java/com/arssg/proyecto_odisea_xxi/LoginActivity.java
+++ app/src/main/java/com/arssg/proyecto_odisea_xxi/LoginActivity.java
@@ -6,8 +6,15 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button btnLogin;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +25,33 @@ public class LoginActivity extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.editTextEmail);
+        password = (EditText) findViewById(R.id.editTextPassword);
+        btnLogin = (Button) findViewById(R.id.cirLoginButton);
+        DB = new DBHelper(this);
     }
 
     public void onLoginClick(View view) {
         startActivity(new Intent(this, RegisterActivity.class));
         overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+    }
+
+    public void btnSingin(View view) {
+        String correo = email.getText().toString();
+        String contraseña = password.getText().toString();
+
+        if (correo.equals("")|| contraseña.equals("") )
+            Toast.makeText(LoginActivity.this, "Favor de no dejar campos vacios", Toast.LENGTH_SHORT).show();
+        else{
+            Boolean checkemailpass = DB.checkemailpassword(correo, contraseña);
+            if(checkemailpass == true){
+                Toast.makeText(LoginActivity.this, "¡Ingreso exitoso!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), pantalla_menu.class);
+                startActivity(intent);
+            }else {
+                Toast.makeText(LoginActivity.this, "¡Datos incorrectos!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/arssg/proyecto_odisea_xxi/RegisterActivity.java
+++ app/src/main/java/com/arssg/proyecto_odisea_xxi/RegisterActivity.java
@@ -8,8 +8,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText username, email, mobile, password;
+    Button signup;
+    TextView signin;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,15 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         changeStatusColorBar();
+
+        username = (EditText) findViewById(R.id.editTextName);
+        email = (EditText) findViewById(R.id.editTextEmail);
+        mobile = (EditText) findViewById(R.id.editTextMobile);
+        password = (EditText) findViewById(R.id.editTextPassword);
+        signup = (Button) findViewById(R.id.cirRegisterButton);
+        signin = (TextView) findViewById(R.id.signInRegister);
+        DB = new DBHelper(this);
+
     }
 
     public void changeStatusColorBar() {
@@ -33,5 +51,32 @@ public class RegisterActivity extends AppCompatActivity {
     public void onRegisterClick(View view) {
         startActivity(new Intent(this, LoginActivity.class));
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_in_left);
+    }
+
+    public void btnSingUp(View view) {
+        String usuario = username.getText().toString();
+        String correo = email.getText().toString();
+        String telefono = mobile.getText().toString();
+        String contraseña = password.getText().toString();
+
+        if (usuario.equals("")|| correo.equals("")||telefono.equals("")||contraseña.equals("") )
+            Toast.makeText(RegisterActivity.this, "Favor de no dejar campos vacios", Toast.LENGTH_SHORT).show();
+        else {
+            Boolean checkuser = DB.checkusernamepasswordemailphone(usuario, contraseña, telefono, correo);
+            if (checkuser == false){
+                Boolean insert = DB.insertData(usuario, contraseña, telefono, correo);
+                if (insert == true){
+                    Toast.makeText(RegisterActivity.this, "¡Registro exitoso!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(RegisterActivity.this, "¡Inicie de nuevo sesión!", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(RegisterActivity.this, "Registro fallido", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+                Toast.makeText(RegisterActivity.this, "Usuario ya existente, ingrese otro por favor", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
